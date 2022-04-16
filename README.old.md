35e4702e927394a7eefd8db655b47d2d3b54fce7
Sajadmhy <sajad.mahyaei@gmail.com>
2022-04-16 06:23:10 -0400

    Setup basic react files

---
+++ README.old.md
@@ -0,0 +1,4 @@
+# task-app
+This is a Task App witch handle inputs and render lists.
+
+This project is done with ReactJS.
---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>
+          List:
+          <input type="text" value={} />
+        </label>
+         <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
