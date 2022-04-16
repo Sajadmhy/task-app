8912b0e22c395583d8c510c6f3b81dbc23a48777
Sajadmhy <sajad.mahyaei@gmail.com>
2022-04-16 09:59:35 -0400

    add functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
+import React, {Component} from "react";
+import uniqid from "uniqid";
+import Overview from "./components/Overview";
+
+class App extends Component {  
+  constructor() {
+    super();
+    this.state = {
+      task: {
+        text: "",
+        id: uniqid()
+      },
+      tasks:[],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task : {
+        text: e.target.value,
+        id: this.state.task.id,
+      }
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { 
+        text: "",
+        id: uniqid()
+      },
+    });
+  };
 
 
+  render() {
+    const { task, tasks } = this.state;
 
-function App() {
-  return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <label>
-          List:
-          <input type="text" value={} />
-        </label>
-         <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
+    return (
+      <div>
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">
+            Enter task
+          </label>
+          <input type="text" id="taskInput" onChange={this.handleChange} />
+          <button type="submit">
+            Add Task
+          </button>
+        </form>
+        <Overview tasks={tasks} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Overview.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Overview(props) {
+const { tasks } = props;
+
+    return (
+        <ul>
+            {tasks.map((task) => {
+                return <li key={task.id}>{task.text}</li>
+            })}
+        </ul>
+    );
+};
+
+export default Overview;
